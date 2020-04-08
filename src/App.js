41c19b5979c689d52d3c563097d55afc6504dcd5
Andrew <andrewriznykua93@gmail.com>
2020-04-07 23:05:29 -0500

    fullcalendar imported

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+import '@fullcalendar/core/main.css';
+import '@fullcalendar/daygrid/main.css';
+import '@fullcalendar/timegrid/main.css';
+
+import './App.css';
+import Main from './components/Main';
+
+const App = () => {
   return (
     <div className="App">
-
+      <Main />
     </div>
   );
 }
