cff0c353c52d28bc61c260c42327862b34ce39ab
Andrew <andrewriznykua93@gmail.com>
2020-04-08 17:04:33 -0500

    useContext hook added and getEvents action

--- src/components/calendar/Calendar.jsx
+++ src/components/calendar/Calendar.jsx
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
 import interactionPlugin from '@fullcalendar/interaction';
 
+import AppContext from '../../context/App/appContext';
+
 const Calender = () => {
+  const appContext = useContext(AppContext);
+  const { events, getEvents } = appContext;
+
+  useEffect(() => {
+    getEvents();
+    // eslint-disable-next-line
+  }, [events])
+
   return (
     <div className="col-lg-9">
       <div>
@@ -16,6 +26,7 @@ const Calender = () => {
             center: "title",
             right: "dayGridMonth,timeGridWeek,timeGridDay"
           }}
+          events={events}
         />
       </div>
     </div>
