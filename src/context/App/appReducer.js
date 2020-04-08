2f2e9b73ebe4b74c141b9df957f16dfbf8434bcd
Andrew <andrewriznykua93@gmail.com>
2020-04-08 17:11:54 -0500

    reducer for events created

---
+++ src/context/App/appReducer.js
@@ -0,0 +1,22 @@
+import { ADD_EVENT, GET_EVENTS, SELECT_EVENT } from '../types';
+
+export default (state, action) => {
+  switch(action.type) {
+    case ADD_EVENT:
+      return {
+        ...state,
+        events: action.payload
+      }
+    case GET_EVENTS:
+      return {
+        ...state,
+        events: action.payload
+      }
+    case SELECT_EVENT:
+      return {
+        ...state,
+        selectedEvent: action.payload
+      }
+    default: return state;
+  }
+}
